ba380c9f65c71dafa583868e1da3162d1bf64112
clandrade <cleisson@outlook.com.br>
2024-01-23 12:09:26 -0300

    Doc Map Rhodes

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+this_is_map "yes"
+
+client_scripts {
+  'client.lua',
+}
+
+author '<4NDR4D3/>'
+description 'Andrade-Rhodes - https://discord.gg/fBAQTBRvat'
+version '1.0.9'
